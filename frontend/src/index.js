9310b63f7526b3fb4f440ae5d6567fd6ef3a3d9a
jakub-ner <kubaner1@gmail.com>
2023-09-30 20:59:58 +0200

    routes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,12 +2,9 @@ import React from 'react';
 import {createRoot} from 'react-dom/client';
 
 import './index.css';
-// import {Home} from './screens/Home';
-import Playground from './screens/Playground/Playground';
-import {CourseGenerator} from './screens/CourseGenerator';
-import {ProfilPage} from './screens/ProfilPage/ProfilPage';
+import App from './App';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
-root.render(<CourseGenerator />);
+root.render(<App />);
