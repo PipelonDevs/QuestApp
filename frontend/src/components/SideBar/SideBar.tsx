bd0452cb4534e60e183e7c61734cd8c8a0c7ff24
adam <apawlowski@fream.pl>
2023-09-30 22:37:53 +0200

    navi + ignore

--- frontend/src/components/SideBar/SideBar.tsx
+++ frontend/src/components/SideBar/SideBar.tsx
@@ -5,6 +5,7 @@ Please share your feedback here: https://form.asana.com/?k=uvp-HPgd3_hyoXRBw1IcN
 
 import React from "react";
 import "./style.css";
+import {useNavigate} from "react-router-dom";
 
 interface Props {
   rectangleClassName: any;
@@ -21,9 +22,13 @@ export const SideBar = ({
   divClassNameOverride,
   rectangleClassName1,
 }: Props): JSX.Element => {
+  const navigate = useNavigate();
+
   return (
     <div className="side-bar">
-      <div className="notification-button">
+      <div
+        className="notification-button"
+        onClick={() => console.log("Powiadomienia")}>
         <div className={`rectangle ${rectangleClassName}`} />
         <img
           className="notification-bell"
@@ -31,19 +36,21 @@ export const SideBar = ({
           src="/img/notificationbell-1.png"
         />
       </div>
-      <div className="new-course-button">
+      <div
+        className="new-course-button"
+        onClick={() => navigate("/stworz-kurs", {})}>
         <div className={`rectangle ${rectangleClassNameOverride}`} />
         <img className="image" alt="Image" src="/img/image-7.png" />
       </div>
-      <div className="discord-button">
+      <div className="discord-button" onClick={() => console.log("Discord")}>
         <div className={`rectangle ${divClassName}`} />
         <img className="img" alt="Image" src="/img/image-6.png" />
       </div>
-      <div className="div">
+      <div className="div" onClick={() => navigate("/profil", {})}>
         <div className={`rectangle ${divClassNameOverride}`} />
         <img className="image-2" alt="Image" src="/img/image-5.png" />
       </div>
-      <div className="discord-button-2" onClick={() => console.log("Discord")}>
+      <div className="discord-button-2" onClick={() => navigate("/", {})}>
         <div className={`rectangle ${rectangleClassName1}`} />
         <img className="image-3" alt="Image" src="/img/image-4.png" />
       </div>
